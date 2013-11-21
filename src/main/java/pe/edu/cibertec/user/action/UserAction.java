6572fdbf38fafdfb8c13adc9408721590b112b02
Francisco <Francisco@hdelgado-SANTEX>
2013-11-20 19:03:07 -0500

    Lab - Continuacion

--- src/main/java/pe/edu/cibertec/user/action/UserAction.java
+++ src/main/java/pe/edu/cibertec/user/action/UserAction.java
@@ -4,7 +4,9 @@ import pe.edu.cibertec.user.bo.UserBo;
 import com.opensymphony.xwork2.ActionSupport;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import pe.edu.cibertec.model.Role;
 import pe.edu.cibertec.model.User;
+import pe.edu.cibertec.service.RoleService;
 import pe.edu.cibertec.service.UserService;
 
 public class UserAction extends ActionSupport {
@@ -12,11 +14,42 @@ public class UserAction extends ActionSupport {
     @Autowired
     private UserService service;
     
+    @Autowired
+    private RoleService roleService;
+    
     private List<User> usuarios;
+    
+    private List<Role> roles;
+    
+    private User usuario;
     
     private Integer id;
 
-    public List<User> getUsuarios() {
+   
+
+    public String listarUsuarios() throws Exception {
+
+        usuarios = service.list();
+        return SUCCESS;
+    }
+    
+    public String nuevoUsuario() {
+        usuario = new User();
+        roles = roleService.listRoles();
+        return SUCCESS;
+    }
+
+    public String insertarUsuario() {
+        service.insert(usuario);
+        return SUCCESS;
+    }
+
+    public String eliminarUsuario() {
+        service.delete(usuario);
+        return SUCCESS;
+    }
+    
+     public List<User> getUsuarios() {
         return usuarios;
     }
 
@@ -31,18 +64,20 @@ public class UserAction extends ActionSupport {
     public void setId(Integer id) {
         this.id = id;
     }
-
-    public String listarUsuarios() throws Exception {
-
-        usuarios = service.list();
-        return SUCCESS;
+    
+    public User getUsuario() {
+        return usuario;
     }
 
-    public String insertarUsuario() {
-        return SUCCESS;
+    public void setUsuario(User usuario) {
+        this.usuario = usuario;
+    }
+    
+    public List<Role> getRoles() {
+        return roles;
     }
 
-    public String eliminarUsuario() {
-        return SUCCESS;
+    public void setRoles(List<Role> roles) {
+        this.roles = roles;
     }
 }
